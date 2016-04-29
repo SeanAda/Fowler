5a0978a6f4b11c8c00fe8bfa21db3dd11756d3f1
Adams <D062403@global.corp.sap>
2016-04-29 16:09:24 +0200

    Replace Conditional with Polymorphism:

--- src/NewReleasePrice.java
+++ src/NewReleasePrice.java
@@ -4,4 +4,9 @@ public class NewReleasePrice extends Price {
 	int getPriceCode() {
 		 return Movie.NEW_RELEASE;
 	 }
+	
+	double getCharge(int daysRented) {
+		double result = daysRented * 3;
+		return result;
+	}
 }
