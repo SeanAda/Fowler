2d709831552e6907b9e03106103c876cafa4ac5a
Adams <D062403@global.corp.sap>
2016-04-29 15:42:56 +0200

    getFrequentRenterPoints()

--- src/Customer.java
+++ src/Customer.java
@@ -26,10 +26,7 @@ class Customer {
         	Rental each = (Rental) enum_rentals.nextElement();
         	
 			 // add frequent renter points
-			frequentRenterPoints ++;
-			
-			 // add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) frequentRenterPoints ++;
+        	frequentRenterPoints += each.getFrequentRenterPoints();
 			
 			 //show figures for this rental
 			result += "\t" + each.getMovie().getTitle()+ "\t" +
